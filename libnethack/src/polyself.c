4dbab67f9c7d6007be0426050ff4126a598f03b9
nethack.rankin <nethack.rankin>
2015-01-24 16:59:50 -0500

    Apply Dev Team fix, stone-to-flesh case related to C343-222, nethack.rankin, Sat Dec 2 04:44:56 2006 +0000
    If the player is wielding a cockatrice corpse without gloves while
    polyselfed into a stone golem, stone-to-flesh could result in the
    player still wielding the corpse in flesh golem form.
    
    The fix makes the corpse turn the player back into a stone golem,
    once the first transformation is complete.
    
    Added bonus fix:  polyself was meant to exercise constitution, but
    because exercise() was called in the wrong place, it had no effect
    (because exercising most attributes has no impact while polyselfed).
    This commit moves the exercise() call to the correct place in the
    function, so that it has the intended effect.

--- libnethack/src/polyself.c
+++ libnethack/src/polyself.c
@@ -344,6 +344,7 @@ polymon(int mntmp, boolean noisy)
     boolean sticky = sticks(youmonst.data) && u.ustuck &&
         !Engulfed, was_blind = ! !Blind, dochange = FALSE;
     boolean could_pass_walls = Passes_walls;
+    const char *kbuf;
     int mlvl;
 
     if (mvitals[mntmp].mvflags & G_GENOD) {     /* allow G_EXTINCT */
@@ -355,6 +356,12 @@ polymon(int mntmp, boolean noisy)
 
     if (noisy)
         break_conduct(conduct_polyself);     /* KMH, conduct */
+
+    /* exercise used to be at the very end, but only Wis was affected
+     * there since the polymorph was always in effect by then, and
+     * exercising other attributes has no effect when polyselfed. */
+    exercise(A_CON, FALSE);
+    exercise(A_WIS, TRUE);
 
     if (!Upolyd) {
         /* Human to monster; save human stats */
@@ -581,10 +588,19 @@ polymon(int mntmp, boolean noisy)
     }
     turnstate.vision_full_recalc = TRUE;
     see_monsters(FALSE);
-    exercise(A_CON, FALSE);
-    exercise(A_WIS, TRUE);
     if (noisy)
         encumber_msg();
+    
+    /* this final selftouch might trigger a recursize call to polymon()
+     * [stone golem wielding cockatrice corpse and hit by stone-to-flesh,
+     * becomes flesh golem above, now gets turned back into stone golem] */
+    if (Upolyd) {
+        kbuf = msgprintf("polymorphing into %s while wielding",
+                         an(mons[u.umonnum].mname));
+    } else {
+        kbuf = msgprintf("returning to %s form while wielding", urace.adj);
+    }
+    if (!uarmg) selftouch("No longer petrify-resistant, you", kbuf);
     return 1;
 }
 
