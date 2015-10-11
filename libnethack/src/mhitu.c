bf6ff21cfc7d3be7432bb35c74e7c45da6e831e6
Jonadab the Unsightly One <jonadab@columbus.rr.com>
2015-10-11 13:58:28 -0400

    Fully hook up shield skill to player AC.
    Instead of just improving your chance to dodge, wearing a shield at
    basic shield skill or better now actually improves your Def.  At high
    skill levels, this is multiplicative with enchantment (unless the
    enchantment is worse than +1, in which case it is additive, so that
    more skill is still better than less skill).

--- libnethack/src/mhitu.c
+++ libnethack/src/mhitu.c
@@ -407,32 +407,6 @@ mattacku(struct monst *mtmp)
         tmp -= 2;
     if (tmp <= 0)
         tmp = 1;
-    /* If you are wearing a shield, adjust this per your shield skill */
-    if (uarms) {
-        switch ((P_MAX_SKILL(P_SHIELD) == P_ISRESTRICTED) ?
-                P_ISRESTRICTED : P_SKILL(P_SHIELD)) {
-        case P_ISRESTRICTED:
-            tmp += 5;
-            break;
-        case P_UNSKILLED:
-            tmp += 2;
-            break;
-        case P_BASIC:
-            break;
-        case P_SKILLED:
-            tmp -= 2;
-            break;
-        case P_EXPERT:
-            tmp -= 4;
-            break;
-        case P_MASTER:
-            tmp -= 6;
-            break;
-        default:
-            impossible("Unknown shield skill level: %d",
-                       P_SKILL(P_SHIELD));
-        }
-    }
 
     /* make eels visible the moment they hit/miss us */
     if (mdat->mlet == S_EEL && mtmp->minvis && cansee(mtmp->mx, mtmp->my)) {
