61efee7b811a934a8326254afc920d60de669ff7
Jonadab the Unsightly One <jonadab@columbus.rr.com>
2016-01-30 07:57:52 -0500

    Monster hitpoint regeneration:  every 8 turns (rather than every 20).
    Monsters regenerate hitpoints rather slowly; but now it's somewhat
    less slowly than before.

--- libnethack/src/monmove.c
+++ libnethack/src/monmove.c
@@ -116,7 +116,7 @@ onscary(int x, int y, struct monst * mtmp)
 void
 mon_regen(struct monst *mon, boolean digest_meal)
 {
-    if (mon->mhp < mon->mhpmax && (moves % 20 == 0 || regenerates(mon->data)))
+    if (mon->mhp < mon->mhpmax && (moves % 8 == 0 || regenerates(mon->data)))
         mon->mhp++;
     if (mon->mspec_used)
         mon->mspec_used--;
