f0cf7c9d21514e277996b9c6e72917074e1b5166
Jonadab the Unsightly One <jonadab@columbus.rr.com>
2016-08-04 06:26:13 -0400

    Don't use uninitialized pointer when mfind_target finds no target.
    A variable, mtmp2, was being assigned the output of mfind_target, and
    then the returned struct monst * was being used right away under the
    assumption that it was necessarily valid and contained a monster; but
    in fact mfind_target is allowed to not find a target.  Chaos ensued.

--- libnethack/src/options.c
+++ libnethack/src/options.c
@@ -63,8 +63,17 @@ static const struct nh_listitem mode_list[] = {
     {MODE_EXPLORE, "explore"},
     {MODE_WIZARD, "debug"}
 };
-static const struct nh_enum_option mode_spec =
-    { mode_list, listlen(mode_list) };
+static const struct nh_enum_option mode_spec = {
+    mode_list, listlen(mode_list)
+};
+static const struct nh_listitem mode_list_nochallenge[] = {
+    {MODE_NORMAL, "normal"},
+    {MODE_EXPLORE, "explore"},
+    {MODE_WIZARD, "debug"}
+};
+static const struct nh_enum_option mode_spec_nochallenge = {
+    mode_list_nochallenge, listlen(mode_list_nochallenge)
+};
 
 static const struct nh_listitem align_list[] = {
     {0, "lawful"},
@@ -502,7 +511,9 @@ new_opt_struct(void)
 
     nhlib_find_option(options, "name")->s.maxlen = PL_NSIZ;
     nhlib_find_option(options, "seed")->s.maxlen = RNG_SEED_SIZE_BASE64;
-    nhlib_find_option(options, "mode")->e = mode_spec;
+    nhlib_find_option(options, "mode")->e =
+        is_unlocked_feature(UNLOCK_FIELD_OPT, UNLOCKFEAT_OPT_CHALLENGE) ?
+        mode_spec : mode_spec_nochallenge;
     nhlib_find_option(options, "timezone")->e = timezone_spec;
     nhlib_find_option(options, "polyinit")->e = polyinit_spec;
     nhlib_find_option(options, "align")->e = align_spec;
