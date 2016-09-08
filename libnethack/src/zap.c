FredrIQ/fiqhack

Fixed beam wands bypassing invulnerability after wand rework

libnethack/src/zap.c
@@ -117,6 +117,10 @@ bhitm(struct monst *magr, struct monst *mdef, struct obj *otmp, int range)
     if (Engulfed && mdef == u.ustuck)
         reveal_invis = FALSE;
 
+    if (hityou && u.uinvulnerable) {
+        pline(combat_msgc(magr, mdef, cr_miss), "You're unaffected.");
+        return 0;
+    }
     if (hityou)
         action_interrupted();
 

tsadok/nhfourk

(cherry picked from commit a56e893f80880669a41388f04a3136b407f105fa)
Conflicts:
	libnethack/src/zap.c
(cherry picked from commit ac87fb083881c98182a27bc94cbf64916a15a5ef)

libnethack/src/zap.c
@@ -126,6 +126,10 @@ bhitm(struct monst *user, struct monst *mtmp, struct obj *otmp)
     if (Engulfed && mtmp == u.ustuck)
         reveal_invis = FALSE;
 
+    if (mtmp == &youmonst && u.uinvulnerable) {
+        pline(combat_msgc(user, mtmp, cr_miss), "You're unaffected.");
+        return 0;
+    }
     if (mtmp == &youmonst)
         action_interrupted();
 