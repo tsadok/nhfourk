satokiyon/DartHack

Allow some variance in corridors and reduce predictability
The corridors used to head towards the goal using the straightest
possible line, often making a zig-zag beeline. Allow some slight variance,
sometimes going straight instead of turning, reducing the predictability,
and making those monotonously turning corridors less likely.

src/sp_lev.c
@@ -2148,6 +2148,12 @@ schar ftyp, btyp;
         dix = abs(xx - tx);
         diy = abs(yy - ty);
 
+        if ((dix > diy) && diy && !rn2(dix-diy+1)) {
+            dix = 0;
+        } else if ((diy > dix) && dix && !rn2(diy-dix+1)) {
+            diy = 0;
+        }
+
         /* do we have to change direction ? */
         if (dy && dix > diy) {
             register int ddx = (xx > tx) ? -1 : 1;

tsadok/nhfourk

(cherry picked from commit 2ee06dda5a4f1bac6cf8237f8745322d93ab678a)
Conflicts:
	src/sp_lev.c

libnethack/src/sp_lev.c
@@ -1378,6 +1378,12 @@ dig_corridor(struct level * lev, coord * org, coord * dest, boolean nxcor,
         dix = abs(xx - tx);
         diy = abs(yy - ty);
 
+        if ((dix > diy) && diy && !mrn2(dix-diy+1)) {
+            dix = 0;
+        } else if ((diy > dix) && dix && !mrn2(diy-dix+1)) {
+            diy = 0;
+        }
+
         /* do we have to change direction ? */
         if (dy && dix > diy) {
             int ddx = (xx > tx) ? -1 : 1;