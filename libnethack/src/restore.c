7b79349a44018062355bb2cf913836bd162fd9ee
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-10-21 19:39:04 +0200

    Add whereis capabilities. Define WHEREIS to use it
    (cherry picked from commit a380fa4dcc4b8c9ffa6297517909ecc55be69148)
    
    Conflicts:
    	GNUmakefile
    	libnethack/include/extern.h
    	libnethack/src/files.c
    	libnethack/src/topten.c

--- libnethack/src/restore.c
+++ libnethack/src/restore.c
@@ -925,7 +925,9 @@ dorecover(struct memfile *mf)
        change the save file, something that the save code detects as a
        desync. Therefore, this is now the caller's job. */
 
-    /* Success! */
+    /* Success! Update whereis. */
+    update_whereis(TRUE);
+
     mf->pos = temp_pos;
     return 1;
 }
