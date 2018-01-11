d25da31843c2ec5228184961b1ab6eda20cb17dc
Fredrik Ljungdahl <fredde1994@gmail.com>
2018-01-11 06:50:06 +0100

    Properly initialize petnames

--- libnethack/src/options.c
+++ libnethack/src/options.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-06 */
+/* Last modified by Fredrik Ljungdahl, 2018-01-11 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -263,13 +263,13 @@ static const struct nh_option_desc const_options[] = {
      nh_birth_creation, OPTTYPE_ENUM, {.e = ROLE_NONE}},
     {"catname", "Personalization",
      "the name of your (first) cat",
-     nh_birth_creation, OPTTYPE_STRING, {.s = NULL}},
+     nh_birth_creation, OPTTYPE_STRING, {.s = ""}},
     {"dogname", "Personalization",
      "the name of your (first) dog",
-     nh_birth_creation, OPTTYPE_STRING, {.s = NULL}},
+     nh_birth_creation, OPTTYPE_STRING, {.s = ""}},
     {"horsename", "Personalization",
      "the name of your (first) horse",
-     nh_birth_creation, OPTTYPE_STRING, {.s = NULL}},
+     nh_birth_creation, OPTTYPE_STRING, {.s = ""}},
     {"pettype", "Personalization",
      "your preferred initial pet type",
      nh_birth_creation, OPTTYPE_ENUM, {.e = 0}},
