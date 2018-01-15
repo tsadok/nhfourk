ca2a4bb43ce07787375b238559c8ceb933732b84
Alex Smith <ais523@nethack4.org>
2018-01-15 21:54:33 +0000

    Revert "Properly initialize petnames"
    This reverts commit d25da31843c2ec5228184961b1ab6eda20cb17dc.
    
    The petnames are being initialized in the wrong place, introducing
    an inconsistency that could cause problems down the line.
    Initialize them the same way everything else is.

--- libnethack/src/options.c
+++ libnethack/src/options.c
@@ -1,5 +1,5 @@
 /* vim:set cin ft=c sw=4 sts=4 ts=8 et ai cino=Ls\:0t0(0 : -*- mode:c;fill-column:80;tab-width:8;c-basic-offset:4;indent-tabs-mode:nil;c-file-style:"k&r" -*-*/
-/* Last modified by Fredrik Ljungdahl, 2018-01-11 */
+/* Last modified by Alex Smith, 2018-01-15 */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
@@ -263,13 +263,13 @@ static const struct nh_option_desc const_options[] = {
      nh_birth_creation, OPTTYPE_ENUM, {.e = ROLE_NONE}},
     {"catname", "Personalization",
      "the name of your (first) cat",
-     nh_birth_creation, OPTTYPE_STRING, {.s = ""}},
+     nh_birth_creation, OPTTYPE_STRING, {.s = NULL}},
     {"dogname", "Personalization",
      "the name of your (first) dog",
-     nh_birth_creation, OPTTYPE_STRING, {.s = ""}},
+     nh_birth_creation, OPTTYPE_STRING, {.s = NULL}},
     {"horsename", "Personalization",
      "the name of your (first) horse",
-     nh_birth_creation, OPTTYPE_STRING, {.s = ""}},
+     nh_birth_creation, OPTTYPE_STRING, {.s = NULL}},
     {"pettype", "Personalization",
      "your preferred initial pet type",
      nh_birth_creation, OPTTYPE_ENUM, {.e = 0}},
